a10094a7144fe26672f148f10927f5d2edc5d39c
malbonesi <mikealbo@gmail.com>
2013-11-22 23:34:16 +0000

    Update darwin.js

--- darwin.js
+++ darwin.js
@@ -3,6 +3,12 @@ API.on(USER_JOIN, welcomeUser);
 
 //Allow all managers and hosts to control Darwin? Or explicitly specify who can control in this file?
 
+var malboFacts = [
+    'Malbo is super hot.',
+    'Malbo is vegan.',
+    'Malbo is in an awesome band called Winning Monroe'
+    ];
+
 /*Functions for stuff*/
 
 //Check incoming chat messages and do stuff and things
@@ -14,10 +20,13 @@ function parseChat(data) {
             API.sendChat("Meow?");
     }
     */
-    if (data.from.match(/AgLikeSilver/))
-    if (data.message.match(/purr/ig)) {
-        API.sendChat("Meow?");   
+    if (data.from.match(/AgLikeSilver/)){
+        if (data.message.match(/purr/ig)) {
+            API.sendChat("Meow?");   
+        }
     }
+    
+    
 }
 
 //Welcome users as they join the room \o/
