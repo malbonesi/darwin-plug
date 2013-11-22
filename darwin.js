54d9186d6d8909da4784d19e7157ce9ce76d7ba9
malbonesi <mikealbo@gmail.com>
2013-11-22 20:39:01 +0000

    Update darwin.js

--- darwin.js
+++ darwin.js
@@ -3,9 +3,14 @@ API.on(API.CHAT, parseChat);
 //Check incoming chat messages and do stuff and things
 function parseChat(data) {
 
+    /*
     switch (data) {
-        case data.message.match(/pur/ig):
+        case data.message.match(/purr/ig):
             API.sendChat("Meow?");
     }
-
+    */
+    
+    if (data.message.match(/purr/ig)) {
+        API.sendChat("Meow?");   
+    }
 }
