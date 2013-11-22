8422590f86ad9d1749da186ee49cb53445042c6e
malbonesi <mikealbo@gmail.com>
2013-11-22 22:55:22 +0000

    Update darwin.js

--- darwin.js
+++ darwin.js
@@ -1,4 +1,9 @@
 API.on(API.CHAT, parseChat);
+API.on(USER_JOIN, welcomeUser);
+
+//Allow all managers and hosts to control Darwin? Or explicitly specify who can control in this file?
+
+/*Functions for stuff*/
 
 //Check incoming chat messages and do stuff and things
 function parseChat(data) {
@@ -9,8 +14,14 @@ function parseChat(data) {
             API.sendChat("Meow?");
     }
     */
-    
+    if (data.from.match(/AgLikeSilver/))
     if (data.message.match(/purr/ig)) {
         API.sendChat("Meow?");   
     }
+}
+
+//Welcome users as they join the room \o/
+function welcomeUser(user) {
+    
+    API.sendChat('Heya ' + user.username + '! Welcome to Indie Google+!');
 }
