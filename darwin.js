724238a45bc36f99c3808243f21a4c9c4b8472de
malbonesi <mikealbo@gmail.com>
2013-12-21 22:38:25 +0000

    stuff

--- darwin.js
+++ darwin.js
@@ -15,6 +15,8 @@ API.on(API.WAIT_LIST_UPDATE, capWaitList);
 
 var ROOMNAME = 'myroom'; //for welcoming users
 var WAITCAP = 10;
+var GREET = false;
+var FACTS = false;
 
 //Hash of facts
 //Add new users here to include them in the fact list.
@@ -40,8 +42,23 @@ function rng(array) {
 function parseChat(data) {
 
     var msg = data.message; //Just to lessen code
+    
+    //Amanda's commands
+    if (data.from == "malbo"){
+        switch (msg)
+        case "!greetoff":
+            API.sendChat('Turning greet off');
+            break;
+        
+        /*
+        if (msg == "!greetoff") { GREET = false; }
+        if (msg == "!greeton") { GREET = true; } 
+        */
+    } 
 
     if (msg.match(/^-.*facts$/)) { //will probably want to separate out the '-' matching for non-commands
+    
+        if (!FACTS) return;
 
         var person = msg.substr(1, msg.length-6); //take the string between the '-' and 'facts'
     
@@ -59,6 +76,7 @@ function parseChat(data) {
 //Welcome users as they join the room \o/
 function welcomeUser(user) {
     
+    if (!GREET) return;
     API.sendChat('Heya ' + user.username + '! Welcome to ' + ROOMNAME + '!');
 }
 
